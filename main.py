b41094d1b1b8346b80223676d273026d79cb6210
Goose <leafmanplays@gmail.com>
2025-11-09 23:57:48 +1300

    Create api requests for pokemon

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print("test")
+import requests
+
+base_url = "https://pokeapi.co/api/v2/"
+
+def get_pokemon(name):
+    url = f"{base_url}/pokemon/{name}"
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        poke_data = response.json()
+        return poke_data
+    else:
+        print(f"Failed to retrieve data {response.status_code}")
+
+pokemon_name = 'ditto'
+poke_info = get_pokemon(pokemon_name)
+
+
+if poke_info:
+    print(poke_info["name"])
